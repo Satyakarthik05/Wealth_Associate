ad685f9e4139c98d5d257934a5e2fd4809defabb
SatyaKarthik05 <satyakarthikvelivela@gmail.com>
2025-02-12 08:12:45 +0530

    second

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login_screen from "./Login_screen";
+import RegisterScreen from "./Register_screen";
+// import Admin_panel from "./Screens/Admin_panel";
+// import ForgotPassword from "./Screens/ForgetPassword";
+// import OTPVerification from "./Screens/OtpVerification";
+// import New_Password from "./Screens/New_Password";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login_screen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen
+          name="Home"
+          component={Admin_panel}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Forgetpassword"
+          component={ForgotPassword}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="otpscreen"
+          component={OTPVerification}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="newpassword"
+          component={New_Password}
+          options={{ headerShown: false }}
+        /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#F9FAFB",
   },
 });
---
+++ data/ApiUrl.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  "http://ec2-13-201-40-209.ap-south-1.compute.amazonaws.com:3000";
